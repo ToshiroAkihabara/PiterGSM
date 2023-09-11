aecf8139b545b3550cd078e75f533edee7235d8b
ToshiroAkihabara <sojen1970@gmail.com>
2023-09-12 01:37:48 +0300

    Create config with headers and use black-tool

--- decorator.py
+++ decorator.py
@@ -1,10 +1,11 @@
 import time
 
+
 def time_of_working(func):
     def wrapper(*args, **kwargs):
         time_start = time.time()
         func(*args, **kwargs)
         time_end = time.time() - time_start
-        print('Затраченное время: {} секунд'.format(time_end))
-    return wrapper
+        print("Затраченное время: {} секунд".format(time_end))
 
+    return wrapper
--- piterGSM.py
+++ piterGSM.py
@@ -8,206 +8,166 @@ import pagination as pagination
 from bs4 import BeautifulSoup as BS
 from time import sleep
 from decorator import time_of_working as times
-
-cookies = {
-    'PHPSESSID': 'eueq05i6v3r5u50g4l8ritd86j',
-    'BITRIX_SM_CLEARED_CURRENT_CITY_ID': 'Y',
-    'BITRIX_SM_CURRENT_CITY_ID': '85',
-    'BITRIX_SM_SALE_UID': '00df953a17eb33935432b35ab1ee6893',
-    '_gid': 'GA1.2.581852796.1678623940',
-    '__utma': '135143152.577129358.1678623940.1678623940.1678623940.1',
-    '__utmc': '135143152',
-    '__utmz': '135143152.1678623940.1.1.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided)',
-    '__utmt': '1',
-    '_ym_uid': '1678623940685676400',
-    '_ym_d': '1678623940',
-    '_ym_isad': '1',
-    'BITRIX_CONVERSION_CONTEXT_s1': '%7B%22ID%22%3A21%2C%22EXPIRE%22%3A1678654740%2C%22UNIQUE%22%3A%5B%22conversion_visit_day%22%5D%7D',
-    '_ym_visorc': 'w',
-    'tmr_lvid': 'befefedc1320dd6cb5796e3c60b60e1b',
-    'tmr_lvidTS': '1678623945086',
-    'adtech_uid': 'ab81c140-794a-40bb-9331-4c0e2cb3ac9d%3Apitergsm.ru',
-    'top100_id': 't1.6394010.783547749.1678623945151',
-    '_gat_%2Fcatalog%2Fphones%2Fiphone%2F': '1',
-    '_gat_gtag_UA_106821143_2': '1',
-    '_ga_SB0GW1QX9E': 'GS1.1.1678623939.1.1.1678624072.60.0.0',
-    '_ga': 'GA1.2.577129358.1678623940',
-    '_gat_UA-250083750-1': '1',
-    '__utmb': '135143152.9.10.1678623940',
-    'tmr_detect': '1%7C1678624075219',
-    'last_visit': '1678613275246%3A%3A1678624075246',
-    't3_sid_6394010': 's1.678590365.1678623945152.1678624078578.1.15',
-}
-
-headers = {
-    'authority': 'pitergsm.ru',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
-    'cookie': 'PHPSESSID=eueq05i6v3r5u50g4l8ritd86j; BITRIX_SM_CLEARED_CURRENT_CITY_ID=Y; BITRIX_SM_CURRENT_CITY_ID=85; BITRIX_SM_SALE_UID=00df953a17eb33935432b35ab1ee6893; _gid=GA1.2.581852796.1678623940; __utma=135143152.577129358.1678623940.1678623940.1678623940.1; __utmc=135143152; __utmz=135143152.1678623940.1.1.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); __utmt=1; _ym_uid=1678623940685676400; _ym_d=1678623940; _ym_isad=1; BITRIX_CONVERSION_CONTEXT_s1=%7B%22ID%22%3A21%2C%22EXPIRE%22%3A1678654740%2C%22UNIQUE%22%3A%5B%22conversion_visit_day%22%5D%7D; _ym_visorc=w; tmr_lvid=befefedc1320dd6cb5796e3c60b60e1b; tmr_lvidTS=1678623945086; adtech_uid=ab81c140-794a-40bb-9331-4c0e2cb3ac9d%3Apitergsm.ru; top100_id=t1.6394010.783547749.1678623945151; _gat_%2Fcatalog%2Fphones%2Fiphone%2F=1; _gat_gtag_UA_106821143_2=1; _ga_SB0GW1QX9E=GS1.1.1678623939.1.1.1678624072.60.0.0; _ga=GA1.2.577129358.1678623940; _gat_UA-250083750-1=1; __utmb=135143152.9.10.1678623940; tmr_detect=1%7C1678624075219; last_visit=1678613275246%3A%3A1678624075246; t3_sid_6394010=s1.678590365.1678623945152.1678624078578.1.15',
-    'referer': 'https://pitergsm.ru/catalog/phones/iphone/',
-    'sec-ch-ua': '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'same-origin',
-    'sec-fetch-user': '?1',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
-}
+from config import headers
 
 
-def parse_html(count: int, key: str) -> list:      
+def parse_html(count: int, key: str) -> list:
     """Parse and insert datas from html into early created csv files and return massive with data"""
     """Извлекает данные из html страниц и вставляет их в csv файлы, затем возвращает массив данных"""
-    
+
     try:
-        with open(f'data_html/{count}_{key}.html', encoding='utf-8') as file:
+        with open(f"data_html/{count}_{key}.html", encoding="utf-8") as file:
             scr = file.read()
-        
-        soup = BS(scr, 'lxml')
-        data = soup.find_all('div', class_="catalog__list like-cards")
+
+        soup = BS(scr, "lxml")
+        data = soup.find_all("div", class_="catalog__list like-cards")
         all_data = []
         for i in data:
-            cards = i.find_all('div', class_="catalog__item")
+            cards = i.find_all("div", class_="catalog__item")
             for card in cards:
                 data_dict = {}
                 try:
-                    title = card.find('div', class_="prod-card__title").text
-                    available = card.find('div', class_="prod-card__count icon-check-green nodesktop").text
+                    title = card.find("div", class_="prod-card__title").text
+                    available = card.find(
+                        "div", class_="prod-card__count icon-check-green nodesktop"
+                    ).text
                     if not available:
                         raise AttributeError("Нет в наличии")
-                    price = card.find('div', class_="price__now").text.replace('a', ' ').strip()
-                    url_page = 'https://pitergsm.ru' + card.find('div', class_="prod-card").find('a', class_="prod-card__link").get('href')
+                    price = (
+                        card.find("div", class_="price__now")
+                        .text.replace("a", " ")
+                        .strip()
+                    )
+                    url_page = "https://pitergsm.ru" + card.find(
+                        "div", class_="prod-card"
+                    ).find("a", class_="prod-card__link").get("href")
                     data_dict[key] = title, available, price, url_page
-                    
-                    with open(f'csv_files/{key}.csv', 'a', encoding='utf-8') as file:
+
+                    with open(f"csv_files/{key}.csv", "a", encoding="utf-8") as file:
                         writer = csv.writer(file)
-                        writer.writerow(
-                            (
-                            title,
-                            available,
-                            price,
-                            url_page
-                            )
-                        )  
+                        writer.writerow((title, available, price, url_page))
                     all_data.append(data_dict)
-                except AttributeError:  
+                except AttributeError:
                     available = "Нет в наличии"
 
         yield all_data
-    
+
     except KeyboardInterrupt as e:
-        print('Принудительное завершение скрипта!')
-        raise(e)
-        
+        print("Принудительное завершение скрипта!")
+        raise (e)
+
 
 def create_csv() -> csv:
     """"Create the csv table of every catalogs with names of columns"""
     """Создает csv таблицу под каждый каталог и задает названия столбцам"""
-    
+
     try:
-        with open('json_files/pagination.json', encoding='utf-8') as file:
+        with open("json_files/pagination.json", encoding="utf-8") as file:
             massive = json.load(file)
         try:
-            os.mkdir('csv_files') 
-            for key, value in massive.items():   
-                with open(f'csv_files/{key}.csv', 'w', encoding='utf-8') as file:
+            os.mkdir("csv_files")
+            for key, value in massive.items():
+                with open(f"csv_files/{key}.csv", "w", encoding="utf-8") as file:
                     writer = csv.writer(file)
                     writer.writerow(
                         (
-                        "Наименование",
-                        "Доступность",
-                        "Стоимость в рублях",
-                        "Ссылка на карточку"
-                        )
-                    )  
-        except FileExistsError:
-            for key, value in massive.items():   
-                with open(f'csv_files/{key}.csv', 'w', encoding='utf-8') as file:
-                    writer = csv.writer(file)
-                    writer.writerow(
-                        (
-                        "Наименование",
-                        "Доступность",
-                        "Стоимость в рублях",
-                        "Ссылка на карточку"
+                            "Наименование",
+                            "Доступность",
+                            "Стоимость в рублях",
+                            "Ссылка на карточку",
                         )
                     )
-    
+        except FileExistsError:
+            for key, value in massive.items():
+                with open(f"csv_files/{key}.csv", "w", encoding="utf-8") as file:
+                    writer = csv.writer(file)
+                    writer.writerow(
+                        (
+                            "Наименование",
+                            "Доступность",
+                            "Стоимость в рублях",
+                            "Ссылка на карточку",
+                        )
+                    )
+
     except KeyboardInterrupt as e:
-        print('Принудительное завершение скрипта!')
-        raise(e)
+        print("Принудительное завершение скрипта!")
+        raise (e)
 
 
 def create_json() -> json:
     """Save the data with html and extraction the massive of data in json"""
     """Сохраняет данные html страниц с последующим извлечение массива данных в json файл"""
-    
+
     try:
         create_csv()
-        with open('json_files/pagination.json', encoding='utf-8') as file:
+        with open("json_files/pagination.json", encoding="utf-8") as file:
             massive = json.load(file)
-    
+
         try:
-            os.mkdir('data_html')
-            count = 0 
+            os.mkdir("data_html")
+            count = 0
             json_dump = []
             for key, value in massive.items():
                 for url in value:
-                        response = requests.get(url=url, cookies=cookies, headers=headers)
-                        scr = response.text
-                        with open(f'data_html/{count}_{key}.html', 'w', encoding='utf-8') as file:
-                            file.write(scr)
-                        parse_html(count, key)
-                        for data in parse_html(count, key):
-                            json_dump.append(data)
-                        sleep(random.randrange(2,3))
-                        os.remove(f'data_html/{count}_{key}.html')
-                        count += 1
-            with open('json_files/piterGSM.json', 'a', encoding='utf-8') as f:
+                    response = requests.get(url=url, headers=headers)
+                    scr = response.text
+                    with open(
+                        f"data_html/{count}_{key}.html", "w", encoding="utf-8"
+                    ) as file:
+                        file.write(scr)
+                    parse_html(count, key)
+                    for data in parse_html(count, key):
+                        json_dump.append(data)
+                    sleep(random.randrange(2, 3))
+                    os.remove(f"data_html/{count}_{key}.html")
+                    count += 1
+            with open("json_files/piterGSM.json", "a", encoding="utf-8") as f:
                 json.dump(json_dump, f, indent=4, ensure_ascii=False)
-        
+
         except FileExistsError:
-            count = 0 
+            count = 0
             json_dump = []
             for key, value in massive.items():
                 for url in value:
-                        response = requests.get(url=url, cookies=cookies, headers=headers)
-                        scr = response.text
-                        with open(f'data_html/{count}_{key}.html', 'w', encoding='utf-8') as file:
-                            file.write(scr)
-                        parse_html(count, key)
-                        for data in parse_html(count, key):
-                            json_dump.append(data)
-                        sleep(random.randrange(2,3))
-                        os.remove(f'data_html/{count}_{key}.html')
-                        count += 1
-            with open('json_files/piterGSM.json', 'a', encoding='utf-8') as f:
+                    response = requests.get(url=url, headers=headers)
+                    scr = response.text
+                    with open(
+                        f"data_html/{count}_{key}.html", "w", encoding="utf-8"
+                    ) as file:
+                        file.write(scr)
+                    parse_html(count, key)
+                    for data in parse_html(count, key):
+                        json_dump.append(data)
+                    sleep(random.randrange(2, 3))
+                    os.remove(f"data_html/{count}_{key}.html")
+                    count += 1
+            with open("json_files/piterGSM.json", "a", encoding="utf-8") as f:
                 json.dump(json_dump, f, indent=4, ensure_ascii=False)
-        
+
     except KeyboardInterrupt as e:
-        print('Принудительное завершение скрипта!')
-        os.remove(f'data/{count}_{key}.html')
-        raise(e)
-        
+        print("Принудительное завершение скрипта!")
+        os.remove(f"data/{count}_{key}.html")
+        raise (e)
+
 
 @times
 def main() -> None:
     """Get json and csv files"""
     """Получает json и csv файлы"""
-     
+
     try:
-        create_json()   
+        create_json()
 
     except FileNotFoundError:
         pagination.main()
         sleep(2)
         main()
-    
-    except KeyboardInterrupt as e:
-        print('Принудительное завершение скрипта!')
-        raise(e)
 
-if __name__=='__main__':
+    except KeyboardInterrupt as e:
+        print("Принудительное завершение скрипта!")
+        raise (e)
+
+
+if __name__ == "__main__":
     main()
